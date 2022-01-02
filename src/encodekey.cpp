9de4894c4b8dca6e7102c11a7b89a97934adcc59
Suikan <26223147+suikan4github@users.noreply.github.com>
2022-01-02 18:13:20 +0900

    Add EncodeKey() test.
    Death test and normal key.

--- src/encodekey.cpp
+++ src/encodekey.cpp
@@ -22,93 +22,93 @@ namespace rpn_engine
                 switch (key_code)
                 {
                     // Row 0
-                case 0x0000: // 0
+                case 0x0002: // 0
                     ret_val = Op::num_a;
                     break;
                 case 0x0001: // X->M
                     // do nothing
                     break;
-                case 0x0002: // CE
+                case 0x0000: // CE
                     // do nothing
                     break;
                     // Row 1
-                case 0x0100: // 4
+                case 0x0102: // 4
                     ret_val = Op::num_e;
                     break;
                 case 0x0101: // 7
                     ret_val = Op::logical_shift_left;
                     break;
-                case 0x0102: // N.A.
+                case 0x0100: // N.A.
                     assert(false);
                     break;
                     // Row 2
-                case 0x0200: // 1
+                case 0x0202: // 1
                     ret_val = Op::num_b;
                     break;
                 case 0x0201: // 8
                     ret_val = Op::logical_shift_right;
                     break;
-                case 0x0202: // CM
+                case 0x0200: // CM
                     // do nothing
                     break;
                     // Row 3
-                case 0x0300: // %
+                case 0x0302: // %
                     ret_val = Op::bit_not;
                     break;
                 case 0x0301: // 5
                     ret_val = Op::num_f;
                     break;
-                case 0x0302: // .
+                case 0x0300: // .
                     ret_val = Op::period;
                     break;
                     // Row 4
-                case 0x0400: // 2
+                case 0x0402: // 2
                     ret_val = Op::num_c;
                     break;
                 case 0x0401: // RM
                     ret_val = Op::rotate_pop;
                     break;
-                case 0x0402: // +
+                case 0x0400: // +
                     ret_val = Op::bit_or;
                     break;
                     // Row 5
-                case 0x0500: // =
+                case 0x0502: // =
                     ret_val = Op::undo;
                     break;
                 case 0x0501: // 9
                     // do nothing
                     break;
-                case 0x0502: // C
+                case 0x0500: // C
                     ret_val = Op::clx;
                     break;
                     // Row 6
-                case 0x0600: // N.A.
+                case 0x0602: // N.A.
                     assert(false);
                     break;
                 case 0x0601: // M-
                     // do nothing
                     break;
-                case 0x0602: // /
+                case 0x0600: // /
                     // do nothing
                     break;
                     // Row 7
-                case 0x0700: // 3
+                case 0x0702: // 3
                     ret_val = Op::num_d;
                     break;
                 case 0x0701: // 6
                     ret_val = Op::dec;
                     break;
-                case 0x0702: // *
+                case 0x0700: // *
                     ret_val = Op::bit_and;
                     break;
                     // Row 8
-                case 0x0800: // sqrt
+                case 0x0802: // sqrt
                     // do nothing
                     break;
                 case 0x0801: // M+
                     ret_val = Op::func;
                     break;
-                case 0x0802: // -
+                case 0x0800: // -
                     ret_val = Op::bit_xor;
                     break;
                 default:
@@ -120,93 +120,93 @@ namespace rpn_engine
                 switch (key_code)
                 {
                     // Row 0
-                case 0x0000: // 0
+                case 0x0002: // 0
                     ret_val = Op::num_0;
                     break;
                 case 0x0001: // X->M
                     ret_val = Op::swap;
                     break;
-                case 0x0002: // CE
+                case 0x0000: // CE
                     // do nothing
                     break;
                     // Row 1
-                case 0x0100: // 4
+                case 0x0102: // 4
                     ret_val = Op::num_4;
                     break;
                 case 0x0101: // 7
                     ret_val = Op::num_7;
                     break;
-                case 0x0102: // N.A.
+                case 0x0100: // N.A.
                     assert(false);
                     break;
                     // Row 2
-                case 0x0200: // 1
+                case 0x0202: // 1
                     ret_val = Op::num_1;
                     break;
                 case 0x0201: // 8
                     ret_val = Op::num_8;
                     break;
-                case 0x0202: // CM
+                case 0x0200: // CM
                     // do nothing
                     break;
                     // Row 3
-                case 0x0300: // %
+                case 0x0302: // %
                     ret_val = Op::bit_neg;
                     break;
                 case 0x0301: // 5
                     ret_val = Op::num_5;
                     break;
-                case 0x0302: // .
+                case 0x0300: // .
                     ret_val = Op::period;
                     break;
                     // Row 4
-                case 0x0400: // 2
+                case 0x0402: // 2
                     ret_val = Op::num_2;
                     break;
                 case 0x0401: // RM
                     ret_val = Op::rotate_pop;
                     break;
-                case 0x0402: // +
+                case 0x0400: // +
                     ret_val = Op::bit_add;
                     break;
                     // Row 5
-                case 0x0500: // =
+                case 0x0502: // =
                     ret_val = Op::enter;
                     break;
                 case 0x0501: // 9
                     ret_val = Op::num_9;
                     break;
-                case 0x0502: // C
+                case 0x0500: // C
                     ret_val = Op::del;
                     break;
                     // Row 6
-                case 0x0600: // N.A.
+                case 0x0602: // N.A.
                     assert(false);
                     break;
                 case 0x0601: // M-
                              // do nothing
                     break;
-                case 0x0602: // /
+                case 0x0600: // /
                     ret_val = Op::bit_div;
                     break;
                     // Row 7
-                case 0x0700: // 3
+                case 0x0702: // 3
                     ret_val = Op::num_3;
                     break;
                 case 0x0701: // 6
                     ret_val = Op::num_6;
                     break;
-                case 0x0702: // *
+                case 0x0700: // *
                     ret_val = Op::bit_mul;
                     break;
                     // Row 8
-                case 0x0800: // sqrt
+                case 0x0802: // sqrt
                     // do nothing
                     break;
                 case 0x0801: // M+
                     ret_val = Op::func;
                     break;
-                case 0x0802: // -
+                case 0x0800: // -
                     ret_val = Op::bit_sub;
                     break;
                 default:
@@ -221,93 +221,93 @@ namespace rpn_engine
                 switch (key_code)
                 {
                     // Row 0
-                case 0x0000: // 0
+                case 0x0002: // 0
                     ret_val = Op::asin;
                     break;
                 case 0x0001: // X->M
                     ret_val = Op::swap_re_im;
                     break;
-                case 0x0002: // CE
+                case 0x0000: // CE
                     ret_val = Op::pi;
                     break;
                     // Row 1
-                case 0x0100: // 4
+                case 0x0102: // 4
                     ret_val = Op::log;
                     break;
                 case 0x0101: // 7
                     ret_val = Op::exp;
                     break;
-                case 0x0102: // N.A.
+                case 0x0100: // N.A.
                     assert(false);
                     break;
                     // Row 2
-                case 0x0200: // 1
+                case 0x0202: // 1
                     ret_val = Op::sin;
                     break;
                 case 0x0201: // 8
                     ret_val = Op::power10;
                     break;
-                case 0x0202: // CM
+                case 0x0200: // CM
                     ret_val = Op::decomplex;
                     break;
                     // Row 3
-                case 0x0300: // %
+                case 0x0302: // %
                     ret_val = Op::atan;
                     break;
                 case 0x0301: // 5
                     ret_val = Op::log10;
                     break;
-                case 0x0302: // .
+                case 0x0300: // .
                     ret_val = Op::acos;
                     break;
                     // Row 4
-                case 0x0400: // 2
+                case 0x0402: // 2
                     ret_val = Op::cos;
                     break;
                 case 0x0401: // RM
                     ret_val = Op::rotate_push;
                     break;
-                case 0x0402: // +
+                case 0x0400: // +
                     ret_val = Op::to_polar;
                     break;
                     // Row 5
-                case 0x0500: // =
+                case 0x0502: // =
                     ret_val = Op::undo;
                     break;
                 case 0x0501: // 9
                     ret_val = Op::conjugate;
                     break;
-                case 0x0502: // C
+                case 0x0500: // C
                     ret_val = Op::clx;
                     break;
                     // Row 6
-                case 0x0600: // N.A.
+                case 0x0602: // N.A.
                     assert(false);
                     break;
                 case 0x0601: // M-
                     // do nothing
                     break;
-                case 0x0602: // /
+                case 0x0600: // /
                     ret_val = Op::inv;
                     break;
                     // Row 7
-                case 0x0700: // 3
+                case 0x0702: // 3
                     ret_val = Op::tan;
                     break;
                 case 0x0701: // 6
                     ret_val = Op::hex;
                     break;
-                case 0x0702: // *
+                case 0x0700: // *
                     ret_val = Op::power;
                     break;
                     // Row 8
-                case 0x0800: // sqrt
+                case 0x0802: // sqrt
                     ret_val = Op::square;
                     break;
                 case 0x0801: // M+
                     ret_val = Op::func;
                     break;
-                case 0x0802: // -
+                case 0x0800: // -
                     ret_val = Op::to_cartesian;
                     break;
                 default:
@@ -319,93 +319,93 @@ namespace rpn_engine
                 switch (key_code)
                 {
                     // Row 0
-                case 0x0000: // 0
+                case 0x0002: // 0
                     ret_val = Op::num_0;
                     break;
                 case 0x0001: // X->M
                     ret_val = Op::swap;
                     break;
-                case 0x0002: // CE
+                case 0x0000: // CE
                     ret_val = Op::eex;
                     break;
                     // Row 1
-                case 0x0100: // 4
+                case 0x0102: // 4
                     ret_val = Op::num_4;
                     break;
                 case 0x0101: // 7
                     ret_val = Op::num_7;
                     break;
-                case 0x0102: // N.A.
+                case 0x0100: // N.A.
                     assert(false);
                     break;
                     // Row 2
-                case 0x0200: // 1
+                case 0x0202: // 1
                     ret_val = Op::num_1;
                     break;
                 case 0x0201: // 8
                     ret_val = Op::num_8;
                     break;
-                case 0x0202: // CM
+                case 0x0200: // CM
                     ret_val = Op::complex;
                     break;
                     // Row 3
-                case 0x0300: // %
+                case 0x0302: // %
                     ret_val = Op::chs;
                     break;
                 case 0x0301: // 5
                     ret_val = Op::num_5;
                     break;
-                case 0x0302: // .
+                case 0x0300: // .
                     ret_val = Op::period;
                     break;
                     // Row 4
-                case 0x0400: // 2
+                case 0x0402: // 2
                     ret_val = Op::num_2;
                     break;
                 case 0x0401: // RM
                     ret_val = Op::rotate_pop;
                     break;
-                case 0x0402: // +
+                case 0x0400: // +
                     ret_val = Op::add;
                     break;
                     // Row 5
-                case 0x0500: // =
+                case 0x0502: // =
                     ret_val = Op::enter;
                     break;
                 case 0x0501: // 9
                     ret_val = Op::num_9;
                     break;
-                case 0x0502: // C
+                case 0x0500: // C
                     ret_val = Op::del;
                     break;
                     // Row 6
-                case 0x0600: // N.A.
+                case 0x0602: // N.A.
                     assert(false);
                     break;
                 case 0x0601: // M-
                     ret_val = Op::change_display;
                     break;
-                case 0x0602: // /
+                case 0x0600: // /
                     ret_val = Op::div;
                     break;
                     // Row 7
-                case 0x0700: // 3
+                case 0x0702: // 3
                     ret_val = Op::num_3;
                     break;
                 case 0x0701: // 6
                     ret_val = Op::num_6;
                     break;
-                case 0x0702: // *
+                case 0x0700: // *
                     ret_val = Op::mul;
                     break;
                     // Row 8
-                case 0x0800: // sqrt
+                case 0x0802: // sqrt
                     ret_val = Op::sqrt;
                     break;
                 case 0x0801: // M+
                     ret_val = Op::func;
                     break;
-                case 0x0802: // -
+                case 0x0800: // -
                     ret_val = Op::sub;
                     break;
                 default:
---
+++ test/test_encodekey.cpp
@@ -0,0 +1,106 @@
+#include "gtest/gtest.h"
+#include "rpnengine.hpp"
+#include <stdexcept>
+
+using rpn_engine::Op;
+
+#define CE 0, 0
+#define XTOM 0, 1
+#define NUM0 0, 2
+#define NA1 1, 0
+#define NUM7 1, 1
+#define NUM4 1, 2
+#define CM 2, 0
+#define NUM8 2, 1
+#define NUM1 2, 2
+#define PERIOD 3, 0
+#define NUM5 3, 1
+#define PERCENT 3, 2
+#define PLUS 4, 0
+#define RM 4, 1
+#define NUM2 4, 2
+#define C 5, 0
+#define NUM9 5, 1
+#define EQUAL 5, 2
+#define DIV 6, 0
+#define MMINUS 6, 1
+#define NA2 6, 2
+#define MUL 7, 0
+#define NUM6 7, 1
+#define NUM3 7, 2
+#define MINUS 8, 0
+#define MPLUS 8, 1
+#define SQRT 8, 2
+
+TEST(EncodeKey, undefined_A)
+{
+    ASSERT_DEATH(rpn_engine::EncodeKey(NA1, false, false), "false");
+}
+
+TEST(EncodeKey, undefined_B)
+{
+    ASSERT_DEATH(rpn_engine::EncodeKey(NA2, false, false), "false");
+}
+
+TEST(EncodeKey, undefined_C)
+{
+    ASSERT_DEATH(rpn_engine::EncodeKey(NA1, false, true), "false");
+}
+
+TEST(EncodeKey, undefined_D)
+{
+    ASSERT_DEATH(rpn_engine::EncodeKey(NA2, false, true), "false");
+}
+
+TEST(EncodeKey, undefined_E)
+{
+    ASSERT_DEATH(rpn_engine::EncodeKey(NA1, true, false), "false");
+}
+
+TEST(EncodeKey, undefined_F)
+{
+    ASSERT_DEATH(rpn_engine::EncodeKey(NA2, true, false), "false");
+}
+
+TEST(EncodeKey, undefined_G)
+{
+    ASSERT_DEATH(rpn_engine::EncodeKey(NA1, true, true), "false");
+}
+
+TEST(EncodeKey, undefined_H)
+{
+    ASSERT_DEATH(rpn_engine::EncodeKey(NA2, true, true), "false");
+}
+
+TEST(EncodeKey, DEC_NORMAL)
+{
+    EXPECT_EQ(rpn_engine::EncodeKey(XTOM, false, false), Op::swap);
+    EXPECT_EQ(rpn_engine::EncodeKey(CM, false, false), Op::complex);
+    EXPECT_EQ(rpn_engine::EncodeKey(RM, false, false), Op::rotate_pop);
+    EXPECT_EQ(rpn_engine::EncodeKey(MMINUS, false, false), Op::change_display);
+    EXPECT_EQ(rpn_engine::EncodeKey(MPLUS, false, false), Op::func);
+
+    EXPECT_EQ(rpn_engine::EncodeKey(NUM7, false, false), Op::num_7);
+    EXPECT_EQ(rpn_engine::EncodeKey(NUM8, false, false), Op::num_8);
+    EXPECT_EQ(rpn_engine::EncodeKey(NUM9, false, false), Op::num_9);
+    EXPECT_EQ(rpn_engine::EncodeKey(DIV, false, false), Op::div);
+    EXPECT_EQ(rpn_engine::EncodeKey(C, false, false), Op::del);
+
+    EXPECT_EQ(rpn_engine::EncodeKey(NUM4, false, false), Op::num_4);
+    EXPECT_EQ(rpn_engine::EncodeKey(NUM5, false, false), Op::num_5);
+    EXPECT_EQ(rpn_engine::EncodeKey(NUM6, false, false), Op::num_6);
+    EXPECT_EQ(rpn_engine::EncodeKey(MUL, false, false), Op::mul);
+    EXPECT_EQ(rpn_engine::EncodeKey(CE, false, false), Op::eex);
+
+    EXPECT_EQ(rpn_engine::EncodeKey(NUM1, false, false), Op::num_1);
+    EXPECT_EQ(rpn_engine::EncodeKey(NUM2, false, false), Op::num_2);
+    EXPECT_EQ(rpn_engine::EncodeKey(NUM3, false, false), Op::num_3);
+    EXPECT_EQ(rpn_engine::EncodeKey(MINUS, false, false), Op::sub);
+    EXPECT_EQ(rpn_engine::EncodeKey(SQRT, false, false), Op::sqrt);
+
+    EXPECT_EQ(rpn_engine::EncodeKey(NUM0, false, false), Op::num_0);
+    EXPECT_EQ(rpn_engine::EncodeKey(PERIOD, false, false), Op::period);
+    EXPECT_EQ(rpn_engine::EncodeKey(PERCENT, false, false), Op::chs);
+    EXPECT_EQ(rpn_engine::EncodeKey(PLUS, false, false), Op::add);
+    EXPECT_EQ(rpn_engine::EncodeKey(EQUAL, false, false), Op::enter);
+}
